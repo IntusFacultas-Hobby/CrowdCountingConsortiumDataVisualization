79eea2e3d6fcaa3b670ebeb40198d154b51bbb72
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2020-10-19 11:46:11 -0400

    Added basic CSV Validation to stagedata

--- core/management/commands/stagedata.py
+++ core/management/commands/stagedata.py
@@ -5,6 +5,32 @@ import os
 import datetime
 
 
+class BadDataFormat(Exception):
+    pass
+
+
+CITY = 0
+LOCATION = 1
+COUNTY = 2
+STATE = 3
+DATE = 5
+ESTIMATE_LOW = 7
+ESTIMATE_BEST = 8
+ESTIMATE_HIGH = 9
+ADJUSTED_LOW = 10
+ADJUSTED_HIGH = 11
+ACTOR = 12
+CLAIM = 13
+EVENT_TYPE = 15
+REPORTED_ARREST = 16
+REPORTED_PARTICIPANT_INJURIES = 17
+REPORTED_POLICE_INJURIES = 18
+REPORTED_PROPERTY_DAMAGE = 19
+SOURCE_1 = 22
+SOURCE_2 = 23
+SOURCE_3 = 24
+
+
 class Command(BaseCommand):
     help = 'Stages available data found in folders. Defaults to data for 2019 and 2020'
 
@@ -60,7 +86,90 @@ class Command(BaseCommand):
             writer = csv.writer(fout, delimiter=",")
             row_count = 0
             for row in csv.reader(fin):
+                numToAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                 if row_count == 0:
+                    # verify the csv is in the appropriate format
+                    if row[CITY] != 'CityTown':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[CITY]} expected to be 'CityTown' found '{row[CITY]}'. Rolling back data consumption.")
+                    if row[LOCATION] != 'Location':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[LOCATION]} expected to be 'Location' found '{row[LOCATION]}'. Rolling back data consumption.")
+                    if row[COUNTY] != 'County':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[COUNTY]} expected to be 'County' found '{row[COUNTY]}'. Rolling back data consumption.")
+                    if row[STATE] != 'StateTerritory':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[STATE]} expected to be 'StateTerritory' found '{row[STATE]}'. Rolling back data consumption.")
+                    if row[DATE] != 'Date':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[DATE]} expected to be 'Date' found '{row[DATE]}'. Rolling back data consumption.")
+                    if row[ESTIMATE_LOW] != 'EstimateLow':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[ESTIMATE_LOW]} expected to be 'EstimateLow' found '{row[ESTIMATE_LOW]}'. Rolling back data consumption.")
+                    if row[ESTIMATE_BEST] != 'BestGuess':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[ESTIMATE_BEST]} expected to be 'BestGuess' found '{row[ESTIMATE_BEST]}'. Rolling back data consumption.")
+                    if row[ESTIMATE_HIGH] != 'EstimateHigh':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[ESTIMATE_HIGH]} expected to be 'EstimateHigh' found '{row[ESTIMATE_HIGH]}'. Rolling back data consumption.")
+                    if row[ADJUSTED_LOW] != 'AdjustedLow':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[ADJUSTED_LOW]} expected to be 'AdjustedLow' found '{row[ADJUSTED_LOW]}'. Rolling back data consumption.")
+                    if row[ADJUSTED_HIGH] != 'AdjustedHigh':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[ADJUSTED_HIGH]} expected to be 'AdjustedHigh' found '{row[ADJUSTED_HIGH]}'. Rolling back data consumption.")
+                    if row[ACTOR] != 'Actor':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[ACTOR]} expected to be 'Actor' found '{row[ACTOR]}'. Rolling back data consumption.")
+                    if row[CLAIM] != 'Claim':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[CLAIM]} expected to be 'Claim' found '{row[CLAIM]}'. Rolling back data consumption.")
+                    if row[EVENT_TYPE] != 'EventType':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[EVENT_TYPE]} expected to be 'EventType' found '{row[EVENT_TYPE]}'. Rolling back data consumption.")
+                    if row[REPORTED_ARREST] != 'ReportedArrests':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[REPORTED_ARREST]} expected to be 'ReportedArrests' found '{row[REPORTED_ARREST]}'. Rolling back data consumption.")
+                    if row[REPORTED_PARTICIPANT_INJURIES] != 'ReportedParticipantInjuries':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[REPORTED_PARTICIPANT_INJURIES]} expected to be 'ReportedParticipantInjuries' found '{row[REPORTED_PARTICIPANT_INJURIES]}'. Rolling back data consumption.")
+                    if row[REPORTED_POLICE_INJURIES] != 'ReportedPoliceInjuries':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[REPORTED_POLICE_INJURIES]} expected to be 'ReportedPoliceInjuries' found '{row[REPORTED_POLICE_INJURIES]}'. Rolling back data consumption.")
+                    if row[REPORTED_PROPERTY_DAMAGE] != 'ReportedPropertyDamage':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[REPORTED_PROPERTY_DAMAGE]} expected to be 'ReportedPropertyDamage' found '{row[REPORTED_PROPERTY_DAMAGE]}'. Rolling back data consumption.")
+                    if row[SOURCE_1] != 'Source1':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[SOURCE_1]} expected to be 'Source1' found '{row[SOURCE_1]}'. Rolling back data consumption.")
+                    if row[SOURCE_2] != 'Source2':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[SOURCE_2]} expected to be 'Source2' found '{row[SOURCE_2]}'. Rolling back data consumption.")
+                    if row[SOURCE_3] != 'Source3':
+                        DataPoint.objects.all().delete()
+                        raise BadDataFormat(
+                            f"Column {numToAlpha[SOURCE_3]} expected to be 'Source3' found '{row[SOURCE_3]}'. Rolling back data consumption.")
+
                     row_count += 1
                     continue
                 if row[COUNTRY_CLAIM].upper() in valid_countries and check_claim(row[CLAIM_COLUMN].upper()):
@@ -75,26 +184,6 @@ class Command(BaseCommand):
         DataPoint.objects.all().delete()
 
         # columns
-        CITY = 0
-        LOCATION = 1
-        COUNTY = 2
-        STATE = 3
-        DATE = 5
-        ESTIMATE_LOW = 7
-        ESTIMATE_BEST = 8
-        ESTIMATE_HIGH = 9
-        ADJUSTED_LOW = 10
-        ADJUSTED_HIGH = 11
-        ACTOR = 12
-        CLAIM = 13
-        EVENT_TYPE = 15
-        REPORTED_ARREST = 16
-        REPORTED_PARTICIPANT_INJURIES = 17
-        REPORTED_POLICE_INJURIES = 18
-        REPORTED_PROPERTY_DAMAGE = 19
-        SOURCE_1 = 22
-        SOURCE_2 = 23
-        SOURCE_3 = 24
 
         # months for iterating
         months = [
@@ -111,8 +200,14 @@ class Command(BaseCommand):
             "November",
             "December"
         ]
-        start_year = options["start"][0]
-        end_year = options["end"][0]
+        try:
+            start_year = options["start"][0]
+        except TypeError:
+            start_year = 2019
+        try:
+            end_year = options["end"][0]
+        except TypeError:
+            end_year = 2020
         for year in range(start_year, end_year + 1):
             for month in months:
                 try:
